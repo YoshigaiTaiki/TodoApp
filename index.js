f778f08cb67502b990d5e796018f23d9b0f09c5e
吉開泰紀 <rotodq0301@yahoo.co.jp>
2024-08-06 11:05:09 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if(todos) {
+    todos.forEach(todo => {
+        add(todo);
+    })
+}
+
+form.addEventListener("submit", function (event){
+    event.preventDefault();
+    add();
+});
+
+function add(todo) {
+    let todoText = input.value;
+
+    if(todo) {
+        todoText = todo;
+    }
+
+    if (todoText) {
+    const li = document.createElement("li");
+    li.innerText = todoText;
+    li.classList.add("list-group-item");
+
+    li.addEventListener("contextmenu", function
+    (event) {
+        event.preventDefault();
+        li.remove();
+        saveDate();
+        li:this.remove();
+        saveDate();
+    });
+
+    li.addEventListener("click", function () {
+        li.classList.toggle
+        ("text-decoration-line-through");
+    })
+
+    ul.appendChild(li);
+    input.value = "";
+    saveDate();
+}
+}
+
+function saveDate() {
+    const lists = document.querySelectorAll("li");
+    let todos = []
+    lists.forEach(list => {
+        todos.push(list.innerText);
+    });
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+}
